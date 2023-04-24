109b32d0cb64973b16d54f612a16dd4a9172395f
als7928 <als7928@daum.net>
2023-04-24 17:32:27 +0900

    dd

--- latent-diffusion/ldm/data/shanghai.py
+++ latent-diffusion/ldm/data/shanghai.py
@@ -7,22 +7,29 @@ from torchvision import transforms
 
 class ShanghaiBase(Dataset):
     def __init__(self,
-                 txt_file,
-                 data_root,
+                 data_dir,
                  size=None,
-                 interpolation="bicubic",
-                 flip_p=0.5
+                 interpolation="bicubic"
+                #  flip_p=0.5
                  ):
-        self.data_paths = txt_file
-        self.data_root = data_root
-        with open(self.data_paths, "r") as f:
-            self.image_paths = f.read().splitlines()
+        # self.data_paths = txt_file
+        self.data_dir = data_dir
+        self.image_paths = []
+        for subdir, dirs, files in os.walk(data_dir):
+            for file in files:
+                if file.endswith(".png"):
+                    self.image_paths.append(os.path.join(subdir, file))
+        # with open(self.data_paths, "r") as f:
+        #     self.image_paths = f.read().splitlines()
+
+
+
         self._length = len(self.image_paths)
-        self.labels = {
-            "relative_file_path_": [l for l in self.image_paths],
-            "file_path_": [os.path.join(self.data_root, l)
-                           for l in self.image_paths],
-        }
+        # self.labels = {
+        #     "relative_file_path_": [l for l in self.image_paths],
+        #     "file_path_": [os.path.join(self.data_dir, l)
+        #                    for l in self.image_paths],
+        # }
 
         self.size = size
         self.interpolation = {"linear": PIL.Image.LINEAR,
@@ -30,14 +37,17 @@ class ShanghaiBase(Dataset):
                               "bicubic": PIL.Image.BICUBIC,
                               "lanczos": PIL.Image.LANCZOS,
                               }[interpolation]
-        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
+        # self.flip = transforms.RandomHorizontalFlip(p=flip_p)
 
     def __len__(self):
         return self._length
 
     def __getitem__(self, i):
-        example = dict((k, self.labels[k][i]) for k in self.labels)
-        image = Image.open(example["file_path_"])
+        image_path = self.image_paths[i]
+        image = Image.open(image_path)
+
+        # example = dict((k, self.labels[k][i]) for k in self.labels)
+        # image = Image.open(example["file_path_"])
         if not image.mode == "RGB":
             image = image.convert("RGB")
 
@@ -52,40 +62,19 @@ class ShanghaiBase(Dataset):
         if self.size is not None:
             image = image.resize((self.size, self.size), resample=self.interpolation)
 
-        image = self.flip(image)
+        # image = self.flip(image)
         image = np.array(image).astype(np.uint8)
-        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
-        return example
+        image = (image / 127.5 - 1.0).astype(np.float32)
+        # example["image"] = (image / 127.5 - 1.0).astype(np.float32)
+        # return example
+        return image
 
 
-class LSUNChurchesTrain(LSUNBase):
+class ShanhaiTrain(ShanghaiBase):
     def __init__(self, **kwargs):
-        super().__init__(txt_file="data/lsun/church_outdoor_train.txt", data_root="data/lsun/churches", **kwargs)
+        super().__init__(data_dir="train/train_data/train_img", **kwargs)
 
 
-class LSUNChurchesValidation(LSUNBase):
-    def __init__(self, flip_p=0., **kwargs):
-        super().__init__(txt_file="data/lsun/church_outdoor_val.txt", data_root="data/lsun/churches",
-                         flip_p=flip_p, **kwargs)
-
-
-class LSUNBedroomsTrain(LSUNBase):
+class ShanhaiValidation(ShanghaiBase):
     def __init__(self, **kwargs):
-        super().__init__(txt_file="data/lsun/bedrooms_train.txt", data_root="data/lsun/bedrooms", **kwargs)
-
-
-class LSUNBedroomsValidation(LSUNBase):
-    def __init__(self, flip_p=0.0, **kwargs):
-        super().__init__(txt_file="data/lsun/bedrooms_val.txt", data_root="data/lsun/bedrooms",
-                         flip_p=flip_p, **kwargs)
-
-
-class LSUNCatsTrain(LSUNBase):
-    def __init__(self, **kwargs):
-        super().__init__(txt_file="data/lsun/cat_train.txt", data_root="data/lsun/cats", **kwargs)
-
-
-class LSUNCatsValidation(LSUNBase):
-    def __init__(self, flip_p=0., **kwargs):
-        super().__init__(txt_file="data/lsun/cat_val.txt", data_root="data/lsun/cats",
-                         flip_p=flip_p, **kwargs)
+        super().__init__(data_dir="train/train_data/test_img",**kwargs)
