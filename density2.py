36d5625b4e83bc07985c64645b4a787527811cfa
als7928 <als7928@daum.net>
2023-05-27 13:20:21 +0900

    dd

--- density2.py
+++ density2.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
 if __name__ == '__main__':
-    img2 = cv2.imread('/data/Capstone/valid/valid_data/valid_density1200/DENSITY_0000.png', cv2.IMREAD_GRAYSCALE)
-    # img2 = cv2.imread('05-14_Clipimg_Crossattn(L2)(540epoch)_sample.png', cv2.IMREAD_GRAYSCALE)
+    multiplied = 30000
+    img2 = cv2.imread('/data/Capstone/valid/valid_data/valid_density_gaus/DENSITY_0020.png', cv2.IMREAD_GRAYSCALE)
+    img3 = cv2.imread('/data/Capstone/test/test_data/test_2imgs/IMG_0020.png_sample.png', cv2.IMREAD_GRAYSCALE)
     # height, width = img2.shape
     # raw_width, raw_height = width, height
     # print(img2.shape)
     # img2 = cv2.resize(img2, (256,256))
-    img2 = img2/1200
+    img2 = img2/multiplied
+    img3 = img3/multiplied
     
-    print(np.sum(img2))
+    print("GT:", np.sum(img2))
+    print("Sample: ", np.sum(img3))
+    print("Error: ", np.sum(img2)-np.sum(img3))
